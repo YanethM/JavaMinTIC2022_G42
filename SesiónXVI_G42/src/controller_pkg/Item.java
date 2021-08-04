f3fddce12e696b6dbdd95e647fc54c5ce2a7a791
YanethM <YanethM@users.noreply.github.com>
2021-08-04 12:06:44 -0500

    Add files via upload

---
+++ SesiónXVI_G42/src/controller_pkg/Item.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller_pkg;
+
+/**
+ *
+ * @author apmejiar
+ */
+public class Item {
+    private int id;
+    private String description;
+
+    public Item(int id, String description) {
+        this.id = id;
+        this.description = description;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+    
+    @Override
+    public String toString(){
+        return getDescription();
+    }
+}
